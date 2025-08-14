32ad2ea878fd593eef13ff8dc448841c41ad9a97
docVoid <81415427+docVoid@users.noreply.github.com>
2025-08-14 08:35:45 +0200

    basic styling (#24)
    * applied same style for watchlist
    
    * watchlist styling #23
    
    * added watchlist component #25

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { WatchlistProvider } from '@/context/watchlistcontext';
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WatchlistProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </WatchlistProvider>
   );
 }
